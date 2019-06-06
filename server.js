4486db37e4475c7c6a18b649e78187515d323fa0
Nick <nicknewburg@gmail.com>
2019-06-06 15:56:08 -0700

    added login and logout feature with cookies

--- server.js
+++ server.js
@@ -18,16 +18,38 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+
 app.get("/", (req, res) => {
-  return res.render("index");
+  console.log(req.session.user)
+  let templateVars = {user: req.session.user};
+  return res.render("index", templateVars);
+})
+
+app.get("/login", (req, res) => {
+  res.render("login")
 })
 
 app.post("/", (req, res) => {
-  knex("users").insert({email: req.body.email, password: req.body.password}).then(result => {
-    res.redirect("/")
+  knex("users").insert({email: req.body.email, password: req.body.password, name: req.body.name}).then(result => {
+     knex("users").where({email: req.body.email}).then(result => {
+      req.session.user = result[0]
+      res.redirect("/")
+   })
   })
+})
+
+app.post("/logout", (req, res) => {
+  req.session.user = ""
+  res.redirect("/")
 })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
