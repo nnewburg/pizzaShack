fac4260650cfe76faef7b52dfebc990bc3fa86f6
Nick <nicknewburg@gmail.com>
2019-07-25 17:22:50 -0700

    added some styling

--- db/seeds/01_items.js
+++ db/seeds/01_items.js
@@ -5,6 +5,7 @@ exports.seed = function(knex, Promise) {
         knex('items').insert({description: 'Pepperoni', price: 10}),
         knex('items').insert({description: 'Wings', price: 5}),
         knex('items').insert({description: 'Vegetarian', price: 13}),
+        knex('items').insert({description: 'Bread', price: 3}),
       ]);
     });
 }
